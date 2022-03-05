6ca7966f7c950ef568cf126bbad86f100665fd02
Justin <justin.i7394@gmail.com>
2022-03-05 16:15:06 -0500

    commit tree 006a9c1f3d851dfc6a62469ebbb3bf354844ba04

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] 
+
+const newCats = [...cats] 
+
+const newCatsOne = [...cats] 
+
+const newCatsRlc = [...cats] 
+
+const newCatsRfc = [...cats]    
+
+function destructivelyAppendCat(name) {
+    cats.push(name) 
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+} 
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name) 
+
+} 
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+} 
+
+function appendCat(name) { 
+   newCats.push(name) 
+   return newCats 
+}  
+
+function prependCat(name) {
+    newCatsOne.unshift(name) 
+    return newCatsOne 
+} 
+
+function removeLastCat(name) {
+    newCatsRlc.pop(name) 
+    return newCatsRlc
+}  
+
+function removeFirstCat(name) {
+    newCatsRfc.shift(name)
+    return newCatsRfc
+}
